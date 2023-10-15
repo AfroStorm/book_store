372a590329fa2ede915efc12fb6fb4a0e372b121
AfroStorm <maloa.elias@yahoo.com>
2023-10-15 11:16:03 +0200

    signals.py correction

--- store_api/signals.py
+++ store_api/signals.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from store_api import models
+from store_api.models import Profile
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -9,15 +9,13 @@ def create_profile(sender, instance, created, **kwargs):
     '''
     Creates a profile and saves the user in itś Customer field
     '''
-
     if created:
-        models.Profile.objects.create(customer=instance)
+        Profile.objects.create(customer=instance)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     '''
-    Saves the newly created UserProfile in the userprofile field of the newly
-    created User
+    Saves the newly created UserProfile
     '''
     instance.profile.save()
